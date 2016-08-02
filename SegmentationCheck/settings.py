9bfd0e90bf71f5129be796bdc5c6503ab8242eaf
xiancan <guopengfei1987v@126.com>
2016-08-02 18:33:07 +0800

    push代码记得 存储切换成qiniu

--- SegmentationCheck/settings.py
+++ SegmentationCheck/settings.py
@@ -116,7 +116,7 @@ CHARACTER_IMAGE_ROOT = MEDIA_ROOT+'character_images/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
-#DEFAULT_FILE_STORAGE = 'qiniustorage.backends.QiniuStorage'
+DEFAULT_FILE_STORAGE = 'qiniustorage.backends.QiniuStorage'
 #STATICFILES_STORAGE = 'qiniustorage.backends.QiniuStaticStorage'
 
 QINIU_ACCESS_KEY= ''
