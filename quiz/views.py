5a9b7809b34c97c322f290e9ae5cafd1b59e247d
xianbu <xianbu@lqsbox>
2016-09-28 21:16:08 +0800

    fix

--- quiz/views.py
+++ quiz/views.py
@@ -13,15 +13,15 @@ from .models import QuizResult, QuizBatch
 
 @login_required(login_url='/segmentation/login/')
 def quiz_batch_create(request):
-    batch_id = QuizBatch.objects.create()
-    offset = random.randint(0, 279)
-    character_statistics = CharacterStatistics.objects.filter(err_cnt__gte=100)[offset]
-    return JsonResponse({u'status': u'ok', u'batch_id': batch_id,
-                         u'char': character_statistics.char})
+    batch = QuizBatch.objects.create(user=request.user)
+    return JsonResponse({u'status': u'ok', u'batch_id': batch.id})
 
 @login_required(login_url='/segmentation/login/')
-def quiz_batch_characters(request, batch_id, char):
-    sql = u"select id, image, is_correct from segmentation_character where char='%s' offset random() * 100 " \
+def quiz_batch_characters(request, batch_id):
+    offset = random.randint(0, 279)
+    character_statistics = CharacterStatistics.objects.filter(err_cnt__gte=100)[offset]
+    char = character_statistics.char
+    sql = u"select id, char, image, is_correct from segmentation_character where char='%s' offset random() * 100 " \
           u"* (select count(*)/100 from segmentation_character where char='%s') limit 100;" \
           % (char, char)
     characters = Character.objects.raw(sql)
@@ -29,6 +29,7 @@ def quiz_batch_characters(request, batch_id, char):
     for char in characters:
         obj = {
             u'id': char.id,
+            u'char': char.char,
             u'image': char.image,
             u'is_correct': char.is_correct,
         }
@@ -66,7 +67,19 @@ def set_correct(request, batch_id):
             quiz_result_lst.append(quiz_result)
         QuizResult.objects.bulk_create(quiz_result_lst)
 
-        data = {'status': 'ok'}
+        count = QuizResult.objects.filter(batch_id=batch_id).count()
+        if count >= 400:
+            right_count = QuizResult.objects.filter(batch_id=batch_id, right_wrong=True).count()
+            score = right_count * 1.0 / count
+            try:
+                batch = QuizBatch.objects.get(id=batch_id)
+                batch.score = score
+                batch.save()
+                data = {'status': 'ok', 'score': score}
+            except:
+                data = {'status': 'ok'}
+        else:
+            data = {'status': 'ok'}
     else:
         data = {'status': 'error'}
     return JsonResponse(data)
