79b05cbdb4a64f27a06115caf188740c159437dc
xiancan <feixeyes@gmail.com>
2016-10-04 11:57:32 +0800

    datatables 點擊事件增加處理函數

--- api/views.py
+++ api/views.py
@@ -52,8 +52,9 @@ class CharacterFilter(filters.FilterSet):
 class CharacterViewSet(viewsets.ModelViewSet):
     serializer_class = CharacterSerializer
     filter_class = CharacterFilter
-    queryset = Character.objects.filter(is_correct__lt=2).\
-        filter(is_correct__gt=-2).order_by('accuracy')
+    queryset = Character.objects.order_by('accuracy')
+    #queryset = Character.objects.filter(is_correct__lt=2).\
+    #    filter(is_correct__gt=-2).order_by('accuracy')
 
 class CharacterStatisticsViewSet(viewsets.ModelViewSet):
     serializer_class = CharacterStatisticsSerializer
