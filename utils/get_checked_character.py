5639ee6ec3bd886c48d734c5ff5f6fff1eac65fe
xiancan <feixeyes@gmail.com>
2016-10-05 21:21:07 +0800

    fix

--- utils/get_checked_character.py
+++ utils/get_checked_character.py
@@ -41,7 +41,7 @@ def main():
     for char, count in char_count_map.iteritems():
         if char in remained_characters and \
                 (char not in char_correct_map_lst[0] or char not in char_correct_map_lst[1] or \
-                             char_correct_map_lst[0][char] < 50 or char_correct_map_lst[1][char] < 50):\
+                             char_correct_map_lst[0][char] < 50 or char_correct_map_lst[1][char] < 50)\
                 and char_count_map[char] > 5:
 
             selected_character_lst.append( (char, count) )
