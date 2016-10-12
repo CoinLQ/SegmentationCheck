6fc44abe4c725c43c42914cb8eaa92ffc61b603d
xiancan <feixeyes@gmail.com>
2016-10-12 14:44:56 +0800

    修改样式

--- characters/urls.py
+++ characters/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     url(r'^task/help$', views.help, name='help'),
     url(r'^set_correct$', views.set_correct, name='set_correct'),
     url(r'^treemap$', views.tree_map),
+    url(r'^dashboard$', views.char_dashboard),
     url(r'^get_marked_char_count$', views.get_marked_char_count, name='get_marked_char_count'),
 ]
