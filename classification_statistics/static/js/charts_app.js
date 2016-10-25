3cc15f8792d74cc6fe251e2739f567b28c23f0c1
lvpython <lvpython@gmail.com>
2016-10-25 16:24:40 +0800

    加入左右重心显示

--- classification_statistics/static/js/charts_app.js
+++ classification_statistics/static/js/charts_app.js
@@ -2,6 +2,7 @@ var svg = d3.select("svg"),
     margin = {top: 20, right: 20, bottom: 30, left: 40},
     width = +svg.attr("width") - margin.left - margin.right,
     height = +svg.attr("height") - margin.top - margin.bottom;
+
 var tooltip = d3.select("body")
   .append("div")
   .style("position", "absolute")
@@ -9,19 +10,22 @@ var tooltip = d3.select("body")
   .style("visibility", "hidden")
   .text("");
 
-var dx =0, dy =0;
+var dx =0, dy =0, vidisor=1;
 if (chunk == 5) {
-  dx = 3; dy = 6;
+  dx = 3; dy = 6; vidisor =200;
 } else if (chunk ==20) {
-  dx = 7; dy = 31;
+  dx = 7; dy = 31; vidisor =12;
 } else if (chunk ==50) {
-  dx = 16; dy = 16;
+  dx = 16; dy = 16; vidisor = 20;
 }
 
 var x = d3.scaleBand().rangeRound([0, width]),
     y = d3.scaleLinear().rangeRound([height, 0]);
     //y = d3.scalePow().exponent(0.5).rangeRound([height, 0]);
     //y = d3.scaleLog(10).rangeRound([height, 0.1]);
+var x2 = d3.scaleLinear().rangeRound([0, width]);
+var y2 = d3.scaleLinear().rangeRound([height, 0]);
+
 var g = svg.append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
@@ -31,7 +35,72 @@ function convert_count( n ){
   //return Math.log10(n);
 }
 
+function draw_gravity_line(val) {
+  var diff = val/vidisor;
+  g.append("line")
+      .attr("transform", "translate("+ dy +",0)")
+      .attr("stroke-width",2)
+      .attr("x1", x2(val -diff))
+      .attr("y1", "-10")
+      .attr("x2", x2(val -diff))
+      .attr("y2", "10")
+      .attr("stroke", "#006600")
+      .attr("class", "gravity")
 
+
+  g.append("text")
+        .attr("transform", "translate("+ dy +",0)")
+        .attr("dx", x2(val-diff))
+        .attr("dy", "-10")
+        .text(val)
+        .attr("text-anchor", "middle")
+      .style("font-size", "0.6em")
+      .style("text-decoration", "underline")
+      .attr("class", "gravity shadow");
+  g.append("text")
+        .attr("transform", "translate("+ dy +",0)")
+        .attr("dx", x2(val-diff))
+        .attr("dy", "-10")
+        .text(val)
+        .attr("text-anchor", "middle")
+      .style("font-size", "0.6em")
+      .style("text-decoration", "underline")
+      .attr("class", "gravity");
+
+}
+
+function draw_title_line(min, max) {
+  var text = "区分度: " + (max-min);
+  g.append("line")
+      .attr("transform", "translate("+ dy +",0)")
+      .attr("stroke-width",1.3)
+      .attr("x1", x2(min- min/vidisor))
+      .attr("y1", "-2")
+      .attr("x2", x2(max-max/vidisor))
+      .attr("y2", "-2")
+      .attr("stroke", "#006600")
+      .attr("marker-end", "url(#r-arrow)")
+      .attr("marker-start", "url(#l-arrow)")
+      .attr("class", "gravity");
+  g.append("text")
+        .attr("transform", "translate("+ dy +",0)")
+        .attr("dx", x2((max-min)/2))
+        .attr("dy", "-7")
+        .text(text)
+        .attr("text-anchor", "middle")
+      .style("font-size", "1em")
+      .style("text-decoration", "underline")
+      .attr("class", "shadow gravity");
+   g.append("text")
+        .attr("transform", "translate("+ dy +",0)")
+        .attr("dx", x2((max-min)/2))
+        .attr("dy", "-7")
+        .text(text)
+        .attr("text-anchor", "middle")
+      .style("font-size", "1em")
+      .style("text-decoration", "underline")
+      .attr("class", "gravity");
+}
 
 function _render_chart(data) {
   var max_y = d3.max(data, function(d) { return d.count; } );
@@ -49,11 +118,12 @@ function _render_chart(data) {
       .attr("transform", "translate("+ dy +",0)")
       .attr("class", "axis axis--y")
       .call(d3.axisLeft(y).tickValues( y.ticks(5)))
-    .append("text")
+  .append("text")
       .attr("transform", "rotate(-90)")
-      .attr("dy", "0.71em")
-      .attr("text-anchor", "end")
-      .text("分类值");
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Frequency");
 
   g.selectAll(".bar")
     .data(data)
@@ -71,6 +141,32 @@ function _render_chart(data) {
       .attr("width", x.bandwidth())
       .attr("height", function(d) { return height - y(convert_count(d.count))});
 
+  defs = svg.append("defs");
+  defs.append("marker")
+        .attr("id", "r-arrow")
+        .attr("markerWidth",10)
+        .attr("refX", 7)
+        .attr("refY", 3)
+        .attr("markerHeight", 10)
+        .attr("orient", "auto")
+        .attr("markerUnits","strokeWidth")
+        .append("path")
+          .attr("d", "M0,0 L0,6 L7,3 z")
+          .attr("dy","-3")
+          .attr("fill","#006600");
+
+  defs.append("marker")
+        .attr("id", "l-arrow")
+        .attr("markerWidth",10)
+        .attr("refX",0)
+        .attr("refY", 3)
+        .attr("markerHeight", 10)
+        .attr("orient", "auto")
+        .attr("markerUnits","strokeWidth")
+        .append("path")
+          .attr("d", "M7,0 L7,6 L0,3 z")
+          .attr("dy","-3")
+          .attr("fill","#006600");
   d3.selectAll("g.axis--x g.tick line").attr("y2", function(d){
       var a = $(this);
       if ( (10*d)%1 || (d==6) ) { //小数被1整除
@@ -80,5 +176,12 @@ function _render_chart(data) {
       else
          return 10;
   });
+
+  draw_gravity_line(charListContainer.r_value)
+  draw_gravity_line(charListContainer.l_value)
+
+  draw_title_line(charListContainer.l_value,charListContainer.r_value)
 };
+
 _render_chart(data);
+
