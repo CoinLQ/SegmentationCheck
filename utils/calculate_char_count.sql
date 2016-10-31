8dbd746b8812717f75faf6dc9e3b2daa0e847bb0
lvpython <lvpython@gmail.com>
2016-10-31 12:55:30 +0800

    update sql

--- utils/calculate_char_count.sql
+++ utils/calculate_char_count.sql
@@ -1,11 +1,12 @@
 ﻿SET SEARCH_PATH TO public;
-INSERT INTO public.segmentation_characterstatistics (char,total_cnt, correct_cnt, err_cnt,uncheck_cnt)
+INSERT INTO public.segmentation_characterstatistics (char,total_cnt, correct_cnt, err_cnt,uncheck_cnt, weight)
 SELECT 
 char,
   count(segmentation_character."char") as total_cnt,
   sum(case when is_correct= 1 then 1 else 0 end) as correct_cnt,
   sum(case when is_correct= -1 then 1 else 0 end) as err_cnt,
-  sum(case when is_correct= 0 then 1 else 0 end) as uncheck_cnt
+  sum(case when is_correct= 0 then 1 else 0 end) as uncheck_cnt,
+  0
 FROM
   public.segmentation_character
 where is_correct = -1 or is_correct =1 or is_correct = 0
