3284238b19d09bb7bd21564aae79a2b829eacad7
lvpython <lvpython@gmail.com>
2016-11-08 17:57:09 +0800

    a

--- apps/settings/production.py
+++ apps/settings/production.py
@@ -1,6 +1,6 @@
 from apps.settings.base import *
 import os
-
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+#The a for fabric
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY",'a')
 SLACK_TOKEN = os.environ.get("SLACK_TOKEN")
 SLACK_USERNAME = os.environ.get("SLACK_USERNAME")
