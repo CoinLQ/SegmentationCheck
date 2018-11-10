609b4bb5dd6c26843bf032da6d93376e54bbca5a
wangwei <3511622339@qq.com>
2018-11-10 15:45:17 +0800

    修改域名 	修改：     apps/segmentation/models.py

--- apps/segmentation/models.py
+++ apps/segmentation/models.py
@@ -175,7 +175,7 @@ class Character(models.Model):
 
         if self.is_integrity == 1:
             return self.local_image_url()
-        server_host = "http://asset-c%d.dzj3000.com" % int(math.ceil(random.random() * 1))
+        server_host = "http://asset-c%d.dzj3000.lqdzj.cn" % int(math.ceil(random.random() * 1))
         return server_host + '/web/character_images/' + self.resource_key().replace(u'.jpg', u'.png')
 
     @property
